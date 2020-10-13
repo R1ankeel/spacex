0c7c210ffc79d9c0c93a833d135562b384d92c53
R1an <r1ankeel@gmail.com>
2020-10-14 01:50:14 +0300

    created few components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import logo from './logo.svg'
+
+import './style.css'
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import Features from "./components/features/Features";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Herlow</h2>
-    </div>
-  );
+      <>
+        <Header/>
+        <Main/>
+        <Features/>
+        <Footer/>
+      </>
+  )
 }
 
 export default App;
